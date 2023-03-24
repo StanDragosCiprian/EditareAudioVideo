a80ec609017e7ca3d4be36b96682739d690bd200
DragosCiprianStan <dragosciprian.sta@ulbsibiu.ro>
2023-03-24 23:29:48 +0200

    Add project files.

---
+++ Proiect/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Proiect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Beta = new System.Windows.Forms.TextBox();
+            this.Alfa = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.gama = new System.Windows.Forms.TextBox();
+            this.GamaValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(929, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(58, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(929, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(58, 181);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(328, 131);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(929, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(929, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(58, 349);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(328, 107);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(604, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Alfa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(604, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Beta";
+            // 
+            // Beta
+            // 
+            this.Beta.Location = new System.Drawing.Point(662, 305);
+            this.Beta.Name = "Beta";
+            this.Beta.Size = new System.Drawing.Size(100, 22);
+            this.Beta.TabIndex = 13;
+            // 
+            // Alfa
+            // 
+            this.Alfa.Location = new System.Drawing.Point(662, 256);
+            this.Alfa.Name = "Alfa";
+            this.Alfa.Size = new System.Drawing.Size(100, 22);
+            this.Alfa.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(929, 389);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // gama
+            // 
+            this.gama.Location = new System.Drawing.Point(662, 349);
+            this.gama.Name = "gama";
+            this.gama.Size = new System.Drawing.Size(100, 22);
+            this.gama.TabIndex = 16;
+            // 
+            // GamaValue
+            // 
+            this.GamaValue.AutoSize = true;
+            this.GamaValue.Location = new System.Drawing.Point(607, 349);
+            this.GamaValue.Name = "GamaValue";
+            this.GamaValue.Size = new System.Drawing.Size(44, 16);
+            this.GamaValue.TabIndex = 17;
+            this.GamaValue.Text = "Gama";
+            this.GamaValue.Click += new System.EventHandler(this.GamaValue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 578);
+            this.Controls.Add(this.GamaValue);
+            this.Controls.Add(this.gama);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Alfa);
+            this.Controls.Add(this.Beta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Beta;
+        private System.Windows.Forms.TextBox Alfa;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox gama;
+        private System.Windows.Forms.Label GamaValue;
+    }
+}
+
---
+++ Proiect/Form1.cs
@@ -0,0 +1,100 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        Image<Bgr, byte> My_Imgae = null;
+        Image<Bgr, byte> outputImage = null;
+        Image<Bgr, byte> g = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                My_Imgae = new Image<Bgr, byte>(openFileDialog.FileName);
+                pictureBox1.Image = My_Imgae.ToBitmap();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (My_Imgae != null)
+            {
+             
+                for (int i = 0; i < My_Imgae.Width / 2; i++)
+                {
+                    for (int j = 0; j < My_Imgae.Height / 2; j++)
+                    {
+                        My_Imgae[j, i] = new Bgr(Color.FromArgb(0, 255, 255, 255));
+                    }
+                }
+                Image<Gray, byte> gray_image = My_Imgae.Convert<Gray, byte>();
+                pictureBox2.Image = gray_image.AsBitmap();
+                gray_image[0, 0] = new Gray(200);
+
+                
+            }
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HistogramViewer hist = new HistogramViewer();
+            hist.HistogramCtrl.GenerateHistograms(My_Imgae, 255);
+            hist.Text = "ce";
+            hist.Show();
+            HistogramViewer hist2 = new HistogramViewer();
+            hist2.HistogramCtrl.GenerateHistograms(outputImage, 255);
+            hist2.Show();
+        }
+
+      
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double alfa = double.Parse( Alfa.Text);
+            double beta = double.Parse(Beta.Text);
+           outputImage = My_Imgae.Mul(alfa)+beta;
+            pictureBox3.Image= outputImage.ToBitmap();
+         
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            outputImage = My_Imgae.Clone();
+            outputImage._GammaCorrect(double.Parse(gama.Text));
+            pictureBox3.Image = outputImage.ToBitmap();
+        }
+
+        private void GamaValue_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
